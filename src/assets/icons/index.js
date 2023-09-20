a1b8ec812b3182161d33d0e179ee6c7e33625479
GizNouv <giznouv@gmail.com>
2023-09-20 17:36:28 +0330

    add assets

---
+++ src/assets/icons/index.js
@@ -0,0 +1,35 @@
+import ArrowRight from './ArrowRight.svg'
+import logo from './logo.svg'
+import menu from './menu.svg'
+import Brain from './Brain.svg'
+import CaretDown from './CaretDown.svg'
+import CrownSimple from './CrownSimple.svg'
+import CurrencyEth from './CurrencyEth.svg'
+import EnvelopeSimple from './EnvelopeSimple.svg'
+import FlyingSaucer from './FlyingSaucer.svg'
+import InstagramLogo from './InstagramLogo.svg'
+import LinkedinLogo from './LinkedinLogo.svg'
+import MapTrifold from './MapTrifold.svg'
+import PhoneCall from './PhoneCall.svg'
+import Quotes from './Quotes.svg'
+import Smiley from './Smiley.svg'
+import TelegramLogo from './TelegramLogo.svg'
+
+export {
+    logo,
+    ArrowRight,
+    menu,
+    FlyingSaucer,
+    EnvelopeSimple,
+    CurrencyEth,
+    CrownSimple,
+    CaretDown,
+    Brain,
+    InstagramLogo,
+    LinkedinLogo,
+    MapTrifold,
+    PhoneCall,
+    Quotes,
+    Smiley,
+    TelegramLogo
+}
