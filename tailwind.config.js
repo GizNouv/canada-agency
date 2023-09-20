03231bc77de0d0bd6482014a2bc24438154aaaab
GizNouv <giznouv@gmail.com>
2023-09-20 17:59:21 +0330

    edit tailwindconf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: ['12px', '16px'],
+      sm: ['14px', '20px'],
+      base: ['16px', '19.5px'],
+      lg: ['18px', '21.94px'],
+      xl: ['20px', '24.38px'],
+      '2xl': ['24px', '29.26px'],
+      '3xl': ['28px', '50px'],
+      '4xl': ['48px', '58px'],
+      '8xl': ['96px', '106px']
+    },
+    extend: {
+      fontFamily: {
+        Guminert: ['Guminert', 'sans-serif'],
+      },
+      colors: {
+        'primary': "#F3F3F7",
+        "alpha-green": "#1B998B",
+        "alpha-orange": "#FF780F",
+        "beta-orange" : "rgba(255, 120, 15, 0.10)",
+        "alpha-blue": "#202232",
+      },
+      boxShadow: {
+        '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
+      },
+      backgroundImage: {
+        'hero': "url('assets/images/hero.png')",
+        'card': "url('assets/images/thumbnail-background.svg')",
+      },
+      screens: {
+        "wide": "1440px"
+      }
+    },
   },
   plugins: [],
 }
-
